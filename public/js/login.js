2723a442ed019dfc30dbcf9fe3ee5fb607f9ca33
Seanders Hancevic <seanhance1@gmail.com>
2018-12-11 20:15:05 -0500

    update user

--- public/js/login.js
+++ public/js/login.js
@@ -94,15 +94,66 @@ $('#submitExistingUser').on('click', function (event) {
 })
 
 $('#user-info').on('click', function (event) {
+    $("#changePass").prop('disabled', false)
     let renderUser = localStorage.getItem('username')
     let renderPass = localStorage.getItem('password')
     console.log('im here')
     let hiddenPass = "*".repeat(renderPass.length)
     $('#modaluser').text(renderUser);
     $('#modalbody').text(hiddenPass);
+    $( "#changePassbtn" ).remove();
+    localStorage.getItem('password')
 })
 
-$('#logout').on('click', function(event){
+
+    $('#changePass').on('click', function(){
+       $(this).prop('disabled', true);
+        $('#changePass-one').append('<input class = "form-control" id = "newPassChange" placeholder="New Password"/>')
+        $('#changePass-two').append('<input class = "form-control" id = "confirmPassChange" placeholder ="Confirm New Password"/>')
+        $('#changePassword').append('<button id = "changePassbtn" onclick="passChangebox()"type = "button" class="btn btn-primary btnhide" data-dismiss="modal">Submit</button>')
+        $(".btnhide").removeClass('hide')
+       
+    })
+    
+   const passChangebox = function(){
+        $( "#newPassChange" ).remove();
+        $( "#confirmPassChange" ).remove();
+        
+   }
+
+    $('body').on('click', '#changePassbtn', function(){
+        const chngPass = {
+            username: localStorage.getItem('username'),
+            password: $('#newPassChange').val().trim(),
+            passwordConf: $('#confirmPassChange').val().trim()
+        };
+        console.log(chngPass)
+        if (chngPass.password !== chngPass.passwordConf) {
+            var err = new Error('Passwords do not match.');
+            err.status = 400;
+            console.log(err)
+            return (err)
+        }
+        else {
+            $.ajax({
+                url: '/api/user',
+                method: 'PUT',
+                data: chngPass
+            }).then(function(newPass){ 
+                if(newPass === null){
+                    var err = new Error('Passwords do not match.');
+                    err.status = 400;
+                    console.log(err)
+                    return (err)
+                }else{
+                    localStorage.setItem('password', chngPass.password)
+                    window.location.reload(); 
+                }
+            })
+        }
+    })
+
+$('#log-out').on('click', function(event){
     localStorage.clear()
-    window.location.href = '/'
+    window.location.href = '/login'
 })
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,93 +5,87 @@ module.exports = function(app){
     app.get('/api/users', function(req, res){
         console.log('hit')
         dbTicket.find({})
-        .then(function(dbUsers){
-            console.log(dbUsers)
-            res.json(dbUsers);
-        })
-        .catch(function(err){
-            console.log(err);
-            res.json(err)
-        })
+            .then(function (dbUsers) {
+                console.log(dbUsers)
+                res.json(dbUsers);
+            })
+            .catch(function (err) {
+                console.log(err);
+                res.json(err)
+            })
     })
-    app.post('/api/users', function(req, res){
+    app.post('/api/users', function (req, res) {
         dbTicket.create(req.body)
-        .then(function(data){
+            .then(function (data) {
 
-            res.json(data);
-        })
+                res.json(data);
+            })
     })
-   
 
-const dbUser = require('../models/user.js');
 
-// Routes for User Login
+    const dbUser = require('../models/user.js');
 
-console.log('route loaded')
+    // Routes for User Login
+
+    console.log('route loaded')
     // GET request: Route for retrieving user from the database.
     app.get('/api/user', function (req, res) { //Works
         console.log('--------retrieving---------');
-        dbUser.findOne({username: req.body.username, password: req.body.password})
-            .then(function (dbUser) { 
-                if(dbUser.length > 0){
-                res.send({success: true})
-                console.log(dbUser)
-                res.redirect('/gittix');
-            } else{
-                res.send('Incorrect Username or Password')
-            }
+        let success;
+        const existingUsers = dbUser.find({ username: req.body.username, password: req.body.password })
+        if (existingUsers.length === 0) {
+            res.send({ success: true })
+            console.log(existingUsers)
+        }
+        else {
+            res.send('Incorrect Username or Password')
+                .catch(function (err) {
+                    console.log(err)
+                    res.json("User does not exist");
+                });
+        }
 
-            })
-            .catch(function (err) {
-                console.log(err)
-                res.json("User does not exist");
-            });
     });
 
     // POST request: Add new user to database
     app.post('/api/user', async function (req, res) {
         let success;
-        const newUsers = await dbUser.find({email: req.body.email, username: req.body.username})
-        
-        if(newUsers.length > 0){
-            console.log('ythhthtth',newUsers)
+        const newUsers = await dbUser.find({ email: req.body.email, username: req.body.username })
+
+        if (newUsers.length > 0) {
+            console.log('ythhthtth', newUsers)
             success = false;
-            res.send({success})
-        }else { 
-            dbUser.create({ email: req.body.email, username: req.body.username, password: req.body.password})
-        .then(function (dbUser) {
-            success = true;
-            console.log(dbUser);
-            res.json({success,})
-        })
-        .catch(function (err) {
-            res.json(err);
-       
-        });}
-        console.log('------Adding to user in mongo');  
+            res.send({ success })
+        } else {
+            dbUser.create({ email: req.body.email, username: req.body.username, password: req.body.password })
+                .then(function (dbUser) {
+                    success = true;
+                    console.log(dbUser);
+                    res.json({ success, })
+                })
+                .catch(function (err) {
+                    res.json(err);
+
+                });
+        }
+        console.log('------Adding to user in mongo');
     });
 
-    app.put("/api/user", function(req, res) {
-        // Update takes in two arguments, an object describing the properties we want to update,
-        // and another "where" object describing the todos we want to update
-        dbUser.update({
-          password: req.body.password,
-        }, {
-          where: {
-            id: req.body.id
-          }
-        })
-          .then(function(dbTodo) {
-            res.json(dbTodo);
-          });
-    
-      });
-
-    // GET request: returns user to login page
-    app.get('/logout', function (request, response) {
-        request.logout();
-        response.redirect('/login');
+    app.put("/api/user", function (req, res) {
+        console.log(req.body)
+        // Update takes in an object describing the properties we want to update, and
+        // we use where to describe which objects we want to update
+        dbUser.update({ username: req.body.username }, { $set: { password: req.body.password } })
+            .then(function (dbTodo) {
+                res.json(dbTodo);
+            })
+            .catch(function (err) {
+                // Whenever a validation or flag fails, an error is thrown
+                // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+                res.json(err);
+            });
     });
+
 
     app.get('/user/signin/callback', function(req, res, next){
         const {query} = req;
